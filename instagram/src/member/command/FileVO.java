90faccc02e8f1036d962f963f63353147ee2c10b
goodlucky1215 <goodlucky1215@naver.com>
2021-05-15 02:31:43 +0900

    파일 업로드하는 다오와 vo추가

---
+++ instagram/src/member/command/FileVO.java
@@ -0,0 +1,46 @@
+package member.command;
+
+public class FileVO {
+    private int fileNo; //파일번호
+    private String fileName; //서버에 저장되는 파일이름
+    private String fileRealName; //파일의 진짜 이름
+    private String MEMBERID; //올린 사람의 아이디
+    private String contentText; //올린 사람이 쓴 글
+    private int read_cnt; //좋아요 수
+	public int getFileNo() {
+		return fileNo;
+	}
+	public void setFileNo(int fileNo) {
+		this.fileNo = fileNo;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getFileRealName() {
+		return fileRealName;
+	}
+	public void setFileRealName(String fileRealName) {
+		this.fileRealName = fileRealName;
+	}
+	public String getMEMBERID() {
+		return MEMBERID;
+	}
+	public void setMEMBERID(String mEMBERID) {
+		MEMBERID = mEMBERID;
+	}
+	public String getContentText() {
+		return contentText;
+	}
+	public void setContentText(String contentText) {
+		this.contentText = contentText;
+	}
+	public int getRead_cnt() {
+		return read_cnt;
+	}
+	public void setRead_cnt(int read_cnt) {
+		this.read_cnt = read_cnt;
+	}
+}
