bf96f02f7ad19c238733d41acb02ae12436febb7
goodlucky1215 <goodlucky1215@naver.com>
2021-06-15 23:21:13 +0900

    Json형식으로 받는 @ResponseBody 적용

--- instagram_springboot/src/main/java/com/example/demo/FindtextHandler.java
+++ instagram_springboot/src/main/java/com/example/demo/FindtextHandler.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
 
@@ -21,7 +22,9 @@ public class FindtextHandler{
 	private LoginService loginservice = null;
 	
 	@RequestMapping("findtext")
-	public void process(HttpServletRequest req, HttpServletResponse res) throws Exception{
+	@ResponseBody
+	public List<Map<String,Object>> process(HttpServletRequest req, HttpServletResponse res) throws Exception{
+		System.out.println("검색한 파일 찾기");
 		res.setContentType("text/html; charset=UTF-8");
 		req.setCharacterEncoding("utf-8");
 		String text = req.getParameter("findtext");
@@ -30,10 +33,11 @@ public class FindtextHandler{
 		List<Map<String,Object>> articles = loginservice.articleList(user.getId(),text);
 
         //DTO 타입의 어레이리스트를 json 형태로 바꿔주는 구문(라이브러리 필수, zip->jar 확장자명 꼭 확인)
-        String gson = new Gson().toJson(articles);
+        //String gson = new Gson().toJson(articles);
 		//둘 다 입력했다면 이제 찾기 시작
         //ajax로 리턴해주는 부분
-        res.getWriter().write(gson);
+        //res.getWriter().write(gson);
+		return articles;
 	}
 
 }
